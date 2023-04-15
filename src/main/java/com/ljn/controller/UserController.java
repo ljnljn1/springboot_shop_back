462eadaaf2d89c9a994802f4de23b50bf85e48d3
26924 <2692469666@qq.com>
2023-04-15 10:07:18 +0800

    第一次提交userController

---
+++ src/main/java/com/ljn/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.ljn.controller;
+
+import com.ljn.po.User;
+import com.ljn.service.UserSercvice;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+  @Author:lijingnan
+  @Date:2023/4/13 16:23
+  @Description:
+*/
+@RequestMapping("/user")
+@RestController
+public class UserController {
+    @Resource
+    private UserSercvice userSercvice;
+    @RequestMapping("/getAllUser")
+    public List<User> getAllUser(){
+       return userSercvice.getAllUser();
+    }
+    @RequestMapping("/userList")
+    public String userList(Model model){
+        model.addAttribute("name","tom");
+        List<User> allUser = userSercvice.getAllUser();
+        model.addAttribute("users",allUser);
+        return "userList";
+    }
+
+    @RequestMapping("/addUser")
+    public  List<String> addUser(@Validated User user, BindingResult br){
+        System.out.println(user);
+        List<ObjectError> allErrors = br.getAllErrors();
+        List<String> errolist=new ArrayList<>();
+        for (ObjectError allError : allErrors) {
+            String defaultMessage = allError.getDefaultMessage();
+            errolist.add(allError.getObjectName()+":"+defaultMessage+":"+allError.getCode());
+        }
+        return errolist;
+    }
+}
